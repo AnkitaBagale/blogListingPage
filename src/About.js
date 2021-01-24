0ee2577f42ae971ae3ef4fb688168f5e0f724027
AnkitaBagale <manali.bagale@gmail.com>
2021-01-24 23:26:15 +0530

    fix: warning

--- src/About.js
+++ src/About.js
@@ -15,12 +15,12 @@ const styleLink = {
             
             <h2>Hi there! I'm Ankita Bagale. Creator of this small App</h2>
             
-            <div className="bodyEm">Currently I am on my journey of learning web development.  I would like to document this beautiful journey. I have created some technical blogs. You can check out those blogs listed on <Link className="link-animation" style={styleLink} to="/myblogs"><a>My blogs</a></Link> page. 
+            <div className="bodyEm">Currently I am on my journey of learning web development.  I would like to document this beautiful journey. I have created some technical blogs. You can check out those blogs listed on <Link className="link-animation" style={styleLink} to="/myblogs">My blogs</Link> page. 
             <br /><br/>
-            I have created a <Link className="link-animation" style={styleLink} to="/search"><a>Search blogs</a></Link> page to check out interesting blogs of other people.</div>
+            I have created a <Link className="link-animation" style={styleLink} to="/search">Search blogs</Link> page to check out interesting blogs of other people.</div>
             <br></br>
             
-            <div className="bodyEm"><strong>Technology:</strong> This app is created using React-JS. Used <a className="link-animation"  href="https://docs.dev.to/api/#tag/articles" target="_blank">Dev.to</a> API to fetch blogs content.</div>
+            <div className="bodyEm"><strong>Technology:</strong> This app is created using React-JS. Used <a className="link-animation" rel="noreferrer" href="https://docs.dev.to/api/#tag/articles" target="_blank">Dev.to</a> API to fetch blogs content.</div>
         </div>
             
     </div>
--- src/Nav.js
+++ src/Nav.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import {React, useState} from 'react';
 import logo from "./logoBgWhite.png";
 import { Link } from "react-router-dom";
 
 
 function Nav() {
 
-    const styleLink = {
-        color: "var(--primary-fontColor)",
-        textDecoration : "none"
-};
+    var [sidebar, setSidebar] = useState("false");
 
     return (
     
       <nav>
+        
         <img className="logo" src={logo} alt="brandLogo"/>
-        <ul>
-            <Link style={styleLink} to="/myblogs">
+        {/* <div className="burger">
+            <div className="line line1"></div>
+            <div className="line line1"></div>
+            <div className="line line1"></div>
+        </div> */}
+        
+        <ul className={sidebar ? "nav-menu active" : "nav-menu"}>
+            <Link className="styleForLink" to="/myblogs">
                 <li className="nav-links link-animation">My blogs</li>
             </Link>
-            <Link style={styleLink} to="/search">
+            <Link className="styleForLink"  to="/search">
                 <li className="nav-links link-animation">Search blogs</li>
             </Link>
-            <Link style={styleLink} to="/about">
+            <Link className="styleForLink" to="/about">
                 <li className="nav-links link-animation">About</li>
             </Link>
         </ul>
+
       </nav>
     
   );
